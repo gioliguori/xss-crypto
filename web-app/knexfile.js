b30af8750511cbb3849b59ee62b8899a79819271
gioliguori <giovanniliguori000@gmail.com>
2025-01-11 14:56:26 +0100

    aggiunto logo

--- web-app/knexfile.js
+++ web-app/knexfile.js
@@ -1,19 +1,18 @@
-const knex = require("knex")({
-  client: "mysql2",
+const knex = require('knex')({
+  client: 'mysql2',
   connection: {
-    host: "localhost",
-    user: "root",
-    database: "applicazione",
+    host: 'localhost',
+    user: 'root',
+    database: 'applicazione',
   },
 });
 
-knex
-  .raw("SELECT 1")
-  .then(() => {
-    console.log("Database connection successful");
-  })
-  .catch((err) => {
-    console.error("Database connection error:", err);
-  });
+knex.raw('SELECT 1')
+    .then(() => {
+      console.log('Database connection successful');
+    })
+    .catch((err) => {
+      console.error('Database connection error:', err);
+    });
 
 module.exports = knex;
